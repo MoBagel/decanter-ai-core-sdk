318e141e6a5b382e96b55eb25c9f9369a0925d00
Kenyo <kenyo3023@gmail.com>
2020-08-19 11:19:27 +0800

    [FEATURES] Remove the 3 test files
    Signed-off-by: Kenyo <kenyo3023@gmail.com>

--- src/decanter/core/core_api/predict_input.py
+++ src/decanter/core/core_api/predict_input.py
@@ -29,8 +29,8 @@ class PredictInput:
 
     """
     def __init__(
-            self, data, experiment, select_model='best', 
-            callback=None, keep_columns=None, threshold=None, version=None):
+            self, data, experiment, select_model='best', callback=None, 
+            keep_columns=None, threshold=None, version=None):
         """
         Init Predict Input
 
