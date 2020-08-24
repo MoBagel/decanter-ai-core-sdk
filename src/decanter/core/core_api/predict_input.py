bd99274a0d36f2b0305c34fdea321b59e962b0ea
Kenyo <kenyo3023@gmail.com>
2020-08-24 11:15:19 +0800

    [FEATRURES] Solve the problem raised on Thursday
    Signed-off-by: Kenyo <kenyo3023@gmail.com>

--- src/decanter/core/core_api/predict_input.py
+++ src/decanter/core/core_api/predict_input.py
@@ -28,8 +28,8 @@ class PredictInput:
 
     """
     def __init__(
-            self, data, experiment, select_model='best', callback=None, 
-            keep_columns=None, threshold=None, version=None):
+            self, data, experiment, select_model='best', select_opt=None, callback=None, 
+                keep_columns=None, threshold=None, version=None):
         """
         Init Predict Input
 
@@ -37,6 +37,17 @@ class PredictInput:
             data (:class:`~decanter.core.jobs.data_upload.DataUpload`): Test data.
             experiment (:class:`~decanter.core.jobs.experiment.Experiment`):
                 Experiment from training.
+            select_model (:obj: `str`, optional): Methods of screening models
+                `best`: based on best CV
+                `model_id`: based on given Model ID
+                `recommendation`: recommended model of the given metric
+                Defaults to 'best'.
+            select_opt (:obj: `str`, optional): Based on the options required by 
+                the select_model.
+                value with select_model case:
+                    `best`          : None
+                    `model_id`      : given the model ID, the ObjectId
+                    `recommendation`: metric, ex: auc ...
             callback (:obj:`str`, optional): A uri to be notified of Decanter Core
                 activity state changes.
             keep_columns (:obj:`list`, optional): The names of the columns
@@ -51,6 +62,7 @@ class PredictInput:
             data_id='tmp_data_id', model_id='tmp_model_id', callback=callback,
             keep_columns=keep_columns, threshold=threshold, version=version)
         self.select_model = select_model
+        self.select_opt = select_opt
 
     def getPredictParams(self):
         """Using pred_body to create the JSON request body for prediction.
@@ -58,28 +70,26 @@ class PredictInput:
         Returns:
             :obj:`dict`
         """
-        if self.select_model == "best":
+        if self.select_model == 'best':
             select_model_id = self.experiment.best_model.id
-        elif self.select_model in self.experiment.models:
-            select_model_id = self.select_model
-        else:
+        elif self.select_model == 'model_id':
+            if self.select_opt in self.experiment.models:
+                select_model_id = self.select_opt
+            else:
+                logger.error('[%s] Invalid input model ID', self.__class__.__name__, self.select_opt)
+        elif self.select_model == 'recommendation':
             for rec in self.experiment.recommendations:
-                if self.select_model == rec['evaluator']:
+                if self.select_opt == rec['evaluator']:
                    select_model_id = rec['model_id']
-
-        if 'select_model_id' not in locals().keys():
-            logger.error('[%s] Invalid input value', self.__class__.__name__, self.select_model)
-        else:
-            setattr(self.pred_body, 'data_id', self.data.id)
-            setattr(self.pred_body, 'model_id', select_model_id)
+            if 'select_model_id' not in locals().keys():
+                logger.error('[%s] Invalid input metric', self.__class__.__name__, self.select_opt)    
+        setattr(self.pred_body, 'data_id', self.data.id)
+        setattr(self.pred_body, 'model_id', select_model_id)
 
         params = json.dumps(
             self.pred_body.jsonable(), cls=CoreBody.ComplexEncoder)
         params = json.loads(params)
         return params
-
-
-
 
 class PredictTSInput(PredictInput):
     """Time series predict input for  PredictTSResult Job.
