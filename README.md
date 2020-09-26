8524adb989a3c7ca0672fc14fd26126a21bc7477
3023 <36863261+MatthewK3023@users.noreply.github.com>
2020-09-26 17:14:45 +0800

    [Bugfix] setup data (#29)
    * [BUGFIX] setup data
    
    -> Fix setup data task and job doesn't complete
    
    Signed-off-by: Noctis Hong <Noctis.Hong@icloud.com>
    
    * [FEATURE] Fix SetupTask issue & Update Document
    
    * [FEATURE] Update Comments
    
    * [bugfix] Update Comments
    
    Co-authored-by: Noctis Hong <Noctis.Hong@icloud.com>
    Co-authored-by: Kenyo <kenyo3023@gmail.com>

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ from decanter import core
 
 core.enable_default_logger()
 client = core.CoreClient(
-        username='{usr}', password='{pwd}', host='{decanter-core-server}')
+        username='gp', password='gp-admin', host='http://host:port')
 
 train_file = open(train_file_path, 'r')
 train_data = client.upload(file=train_file, name="train")
--- src/decanter/core/jobs/data_setup.py
+++ src/decanter/core/jobs/data_setup.py
@@ -15,14 +15,13 @@ from decanter.core.extra import CoreStatus
 from decanter.core.extra.decorators import update
 from decanter.core.extra.utils import check_response, gen_id
 from decanter.core.jobs.job import Job
-from decanter.core.jobs.data_upload import DataUpload
 from decanter.core.jobs.task import SetupTask
 
 
 logger = logging.getLogger(__name__)
 
 
-class DataSetup(DataUpload, Job):
+class DataSetup(Job):
     """DataSetup manage to get the result from data setup.
 
     Handle the execution of setup task in order to setup data to
@@ -42,18 +41,16 @@ class DataSetup(DataUpload, Job):
         completed_at (str): The time the data was completed at.
         name (str): Name to track Job progress, will give default name if None.
     """
-    def __init__(self, train_data=None, setup_params=None, name=None):
+    def __init__(self, setup_input=None, name=None):
         """DataSetup Init.
 
         Args:
-            train_data (:obj:`~decanter.core.jobs.data_setup.DataSetup)
-            setup_params (:obj:`dict`, optional): Settings for setup data.
+            setup_input (:obj:`~decanter.core.jobs.data_setup.DataSetup)
             name (:obj:`str`, optional): Name to track Job progress
         """
-        Job.__init__(
-            self,
-            jobs=[train_data],
-            task=SetupTask(setup_params, name),
+        super().__init__(
+            jobs=[setup_input.data],
+            task=SetupTask(setup_input, name),
             name=gen_id(self.__class__.__name__, name))
         self.accessor = None
         self.schema = None
@@ -63,3 +60,74 @@ class DataSetup(DataUpload, Job):
         self.created_at = None
         self.updated_at = None
         self.completed_at = None
+
+    @classmethod
+    def create(cls, data_id, name=None):
+        """Create data by data_id.
+
+        Args:
+            data_id (str): ObjectId in 24 hex digits
+            name (str): (opt) Name to track Job progress
+
+        Returns:
+            :class:`~decanter.core.jobs.data_setup.DataSetup` object
+        """
+        data = cls()
+        data_resp = check_response(
+            data.core_service.get_data_by_id(data_id)).json()
+        data.update_result(data_resp)
+        data.status = CoreStatus.DONE
+        data.name = name
+        return data
+
+    @update
+    def update_result(self, task_result):
+        """Update from 'result' in Task response."""
+        return
+
+    def show(self):
+        """Show data content.
+
+        Returns:
+            str: Content of setup data.
+        """
+        data_txt = None
+        if self.is_success():
+            data_txt = check_response(
+                self.core_service.get_data_file_by_id(self.id)).text
+        else:
+            logger.error(
+                '[%s] \'%s\' show data failed',
+                self.__class__.__name__, self.name)
+        return data_txt
+
+    def show_df(self):
+        """Show data in pandas dataframe.
+
+        Returns:
+            :class:`pandas.DataFrame`: Content of setup data.
+        """
+        data_df = None
+        if self.is_success():
+            data_csv = check_response(
+                self.core_service.get_data_file_by_id(self.id))
+            data_csv = data_csv.content.decode('utf-8')
+            data_df = pd.read_csv(io.StringIO(data_csv))
+        else:
+            logger.error('[%s get result] fail', self.__class__.__name__)
+        return data_df
+
+    def download_csv(self, path):
+        """DownLoad csv format of the setup data.
+
+        Args:
+            path (str): The path to download csv file.
+        """
+        if self.is_success():
+            data_csv = check_response(
+                self.core_service.get_data_file_by_id(self.id)).text
+            save_csv = open(path, 'w+')
+            save_csv.write(data_csv)
+            save_csv.close()
+        else:
+            logger.error('[%s get result] fail', self.__class__.__name__)
