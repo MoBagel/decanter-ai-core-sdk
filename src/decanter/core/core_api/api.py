ab63115fa54a70f7416e3b17c93f6064969ed7e2
Renee Wu <reneewu@mobagel.com>
2021-10-12 13:46:08 +0800

    fix lint error

--- src/decanter/core/core_api/api.py
+++ src/decanter/core/core_api/api.py
@@ -8,12 +8,11 @@
 :meta private:
 """
 import logging
-
 import requests
-from requests_toolbelt import MultipartEncoder
-from requests.auth import HTTPBasicAuth
-from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
+from requests.auth import HTTPBasicAuth
+from requests_toolbelt import MultipartEncoder
+from urllib3.util.retry import Retry
 import decanter.core as core
 
 logger = logging.getLogger(__name__)
@@ -22,19 +21,17 @@ requests.packages.urllib3.disable_warnings()
 # Retry when having temporary connection issue with CoreX
 # ref: https://stackoverflow.com/a/35504626
 requests_session = requests.Session()
-retries = Retry(total=5,
-                backoff_factor=0.1,
-                status_forcelist=[500, 502, 503, 504])
+retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
 
-requests_session.mount('http://', HTTPAdapter(max_retries=retries))
-requests_session.mount('https://', HTTPAdapter(max_retries=retries))
+requests_session.mount("http://", HTTPAdapter(max_retries=retries))
+requests_session.mount("https://", HTTPAdapter(max_retries=retries))
 
 
 class CoreAPI:
     """Handle sending Decanter Core API requests."""
 
     def __init__(self):
-        self._corex_headers = {'user': 'sdk'}
+        self._corex_headers = {"user": "sdk"}
 
     @property
     def corex_headers(self):
@@ -69,25 +66,42 @@ class CoreAPI:
         basic_auth = HTTPBasicAuth(core.Context.USERNAME, core.Context.PASSWORD)
         url = core.Context.HOST + url
         try:
-            if http == 'GET':
+            if http == "GET":
                 return requests_session.get(url=url, auth=basic_auth, verify=False)
-            if http == 'POST':
+            if http == "POST":
                 return requests_session.post(
-                    url=url, json=json, data=data,
-                    files=files, auth=basic_auth, verify=False, headers=headers)
-            if http == 'PUT':
+                    url=url,
+                    json=json,
+                    data=data,
+                    files=files,
+                    auth=basic_auth,
+                    verify=False,
+                    headers=headers,
+                )
+            if http == "PUT":
                 return requests_session.put(
-                    url=url, json=json, data=data,
-                    files=files, auth=basic_auth, verify=False, headers=headers)
-            if http == 'DELETE':
+                    url=url,
+                    json=json,
+                    data=data,
+                    files=files,
+                    auth=basic_auth,
+                    verify=False,
+                    headers=headers,
+                )
+            if http == "DELETE":
                 return requests_session.delete(
-                    url=url, json=json, data=data,
-                    files=files, auth=basic_auth, verify=False)
+                    url=url,
+                    json=json,
+                    data=data,
+                    files=files,
+                    auth=basic_auth,
+                    verify=False,
+                )
 
-            raise Exception('[Core] No such HTTP Method.')
+            raise Exception("[Core] No such HTTP Method.")
 
         except requests.exceptions.RequestException as err:
-            logger.error('[Core] Request Failed :(')
+            logger.error("[Core] Request Failed :(")
             raise Exception(err)
 
     def get_info(self):
@@ -99,7 +113,7 @@ class CoreAPI:
             class: `Response <Response>` object
 
         """
-        return self.requests_(http='GET', url='/v2/info')
+        return self.requests_(http="GET", url="/v2/info")
 
     def get_data_list(self):
         """Get list of data metadata.
@@ -109,7 +123,7 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='GET', url='/data')
+        return self.requests_(http="GET", url="/data")
 
     def get_data_by_id(self, data_id):
         """Get data metadata.
@@ -119,7 +133,7 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='GET', url='/data/%s' % data_id)
+        return self.requests_(http="GET", url="/data/%s" % data_id)
 
     def get_data_file_by_id(self, data_id):
         """Download csv file of data.
@@ -129,7 +143,7 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='GET', url='/v2/data/%s/file' % data_id)
+        return self.requests_(http="GET", url="/v2/data/%s/file" % data_id)
 
     def post_data_delete(self, **kwargs):
         """Batch delete data.
@@ -139,7 +153,7 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='POST', url='/v2/data/delete', json=kwargs)
+        return self.requests_(http="POST", url="/v2/data/delete", json=kwargs)
 
     def post_upload(self, **kwargs):
         """Upload csv file and setup data.
@@ -152,15 +166,14 @@ class CoreAPI:
         # use suggested package to post a large file up to 10G
         # ref: https://docs.python-requests.org/en/master/user/quickstart/#post-a-multipart-encoded-file
         csv = MultipartEncoder(
-            fields={
-                'csv': (kwargs['filename'], kwargs['file'], kwargs['encoding'])}
+            fields={"csv": (kwargs["filename"], kwargs["file"], kwargs["encoding"])}
         )
         headers = self.corex_headers
-        headers['Content-Type'] = csv.content_type
-        url = '/v2/upload'
-        if 'eda' in kwargs and not kwargs['eda']:
-            url = url + '?eda=true'
-        return self.requests_(http='POST', url=url, data=csv, headers=headers)
+        headers["Content-Type"] = csv.content_type
+        url = "/v2/upload"
+        if "eda" in kwargs and not kwargs["eda"]:
+            url = url + "?eda=true"
+        return self.requests_(http="POST", url=url, data=csv, headers=headers)
 
     def get_tasks_by_id(self, task_id):
         """Get the task by task_id.
@@ -170,7 +183,7 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='GET', url='/v2/tasks/%s' % task_id)
+        return self.requests_(http="GET", url="/v2/tasks/%s" % task_id)
 
     def get_tasks_list(self):
         """Get list of tasks.
@@ -180,7 +193,7 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='GET', url='/tasks')
+        return self.requests_(http="GET", url="/tasks")
 
     def put_tasks_stop_by_id(self, task_id):
         """Stop a running or pending task.
@@ -190,7 +203,9 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='PUT', url='/v2/tasks/%s/stop' % task_id, headers=self.corex_headers)
+        return self.requests_(
+            http="PUT", url="/v2/tasks/%s/stop" % task_id, headers=self.corex_headers
+        )
 
     def post_tasks_setup(self, **kwargs):
         """Setup data reference.
@@ -200,7 +215,9 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='POST', url='/v2/tasks/setup', json=kwargs, headers=self.corex_headers)
+        return self.requests_(
+            http="POST", url="/v2/tasks/setup", json=kwargs, headers=self.corex_headers
+        )
 
     def post_tasks_train(self, **kwargs):
         """Train model from data reference.
@@ -210,7 +227,9 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='POST', url='/v2/tasks/train', json=kwargs, headers=self.corex_headers)
+        return self.requests_(
+            http="POST", url="/v2/tasks/train", json=kwargs, headers=self.corex_headers
+        )
 
     def post_tasks_cluster_train(self, **kwargs):
         """Train model from data reference.
@@ -220,7 +239,12 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='POST', url='/v2/tasks/cluster_train', json=kwargs, headers=self.corex_headers)
+        return self.requests_(
+            http="POST",
+            url="/v2/tasks/cluster_train",
+            json=kwargs,
+            headers=self.corex_headers,
+        )
 
     def post_tasks_auto_ts_train(self, **kwargs):
         """Train time series forecast multi model from data reference.
@@ -231,7 +255,11 @@ class CoreAPI:
             class:`Response <Response>` object
         """
         return self.requests_(
-            http='POST', url='/v2/tasks/auto_ts/train', json=kwargs, headers=self.corex_headers)
+            http="POST",
+            url="/v2/tasks/auto_ts/train",
+            json=kwargs,
+            headers=self.corex_headers,
+        )
 
     def post_tasks_predict(self, **kwargs):
         """Predict from model.
@@ -242,7 +270,11 @@ class CoreAPI:
             class:`Response <Response>` object
         """
         return self.requests_(
-            http='POST', url='/v2/tasks/predict', json=kwargs, headers=self.corex_headers)
+            http="POST",
+            url="/v2/tasks/predict",
+            json=kwargs,
+            headers=self.corex_headers,
+        )
 
     def post_tasks_auto_ts_predict(self, **kwargs):
         """Predict from time series forecast model.
@@ -253,7 +285,11 @@ class CoreAPI:
             class:`Response <Response>` object
         """
         return self.requests_(
-            http='POST', url='/v2/tasks/auto_ts/predict', json=kwargs, headers=self.corex_headers)
+            http="POST",
+            url="/v2/tasks/auto_ts/predict",
+            json=kwargs,
+            headers=self.corex_headers,
+        )
 
     def batch_predict(self, model_id, **kwargs):
         """
@@ -268,11 +304,13 @@ class CoreAPI:
         Returns (json):
         ---------------
         """
-        url = '/v2/models/%s/batch_predict' % model_id
-        if 'timestamp_format' not in kwargs:
-            kwargs['timestamp_format'] = 'yyyy-MM-dd HH:mm:ss'
-        kwargs['version'] = 'v2'
-        return self.requests_(http='POST', url=url, json=kwargs, headers=self.corex_headers)
+        url = "/v2/models/%s/batch_predict" % model_id
+        if "timestamp_format" not in kwargs:
+            kwargs["timestamp_format"] = "yyyy-MM-dd HH:mm:ss"
+        kwargs["version"] = "v2"
+        return self.requests_(
+            http="POST", url=url, json=kwargs, headers=self.corex_headers
+        )
 
     def get_experiments_by_id(self, exp_id):
         """Get experiment metadata.
@@ -282,7 +320,7 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='GET', url='/v2/experiments/%s' % exp_id)
+        return self.requests_(http="GET", url="/v2/experiments/%s" % exp_id)
 
     def get_models_by_id(self, exp_id, model_id):
         """Get model metadata.
@@ -293,7 +331,8 @@ class CoreAPI:
             class:`Response <Response>` object
         """
         return self.requests_(
-            http='GET', url='/v2/experiments/%s/models/%s' % (exp_id, model_id))
+            http="GET", url="/v2/experiments/%s/models/%s" % (exp_id, model_id)
+        )
 
     def get_models_download_by_id(self, model_id):
         """Get model mojo file.
@@ -303,7 +342,7 @@ class CoreAPI:
         Returns:
             class:`Response <Response>` object
         """
-        return self.requests_(http='GET', url='/v2/models/%s/download' % model_id)
+        return self.requests_(http="GET", url="/v2/models/%s/download" % model_id)
 
     def get_multimodels_by_id(self, exp_id, model_id):
         """Get multimodel meta data by model_id which trained in the experiment.
@@ -314,8 +353,8 @@ class CoreAPI:
             class:`Response <Response>` object
         """
         return self.requests_(
-            http='GET',
-            url='/v2/auto_ts/experiments/%s/models/%s' % (exp_id, model_id))
+            http="GET", url="/v2/auto_ts/experiments/%s/models/%s" % (exp_id, model_id)
+        )
 
     def get_worker_count(self):
         """Get counts of each type of worker
@@ -325,9 +364,7 @@ class CoreAPI:
         Returns:
             class: `Response <Response>` object
         """
-        return self.requests_(
-            http='GET',
-            url='/v2/worker/count')
+        return self.requests_(http="GET", url="/v2/worker/count")
 
     def get_worker_status(self):
         """List status of each worker
@@ -337,6 +374,4 @@ class CoreAPI:
         Returns:
             class: `Response <Response>` object
         """
-        return self.requests_(
-            http='GET',
-            url='/v2/worker/status')
+        return self.requests_(http="GET", url="/v2/worker/status")
