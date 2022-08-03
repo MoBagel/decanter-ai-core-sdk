069bd3929fe89f2ed9d738e82586786b9345ba7a
LEON-QA <110512053+LEON-QA@users.noreply.github.com>
2022-08-03 14:44:19 +0800

    Add changed algo names and update the setup version (#51)
    * add the algorithm names which is changed in K8S
    
    * change the setup version
    
    * try to fix linter issue
    
    * black these two files

--- src/decanter/core/enums/algorithms.py
+++ src/decanter/core/enums/algorithms.py
@@ -18,7 +18,8 @@ class Algo(Enum):
     - DeepLearning: Deep Learning.
     - StackedEnsemble: Stacked Ensemble.
     - XGBoost: eXtreme Gradient Boosting.
-    - tpot: Tree-based Pipeline Optimization Tool. (available only after 4.10 deployed with Exodus).
+    - tpot(autotpot): Tree-based Pipeline Optimization Tool. (available only after 4.10 deployed with Exodus).
+    - rfmulticlassifier
 
     TrainTSInput (used by client.train_ts) supported algorithms
 
@@ -27,19 +28,42 @@ class Algo(Enum):
     - GBM: Gradient Boosting Machine.
     - XGBoost: eXtreme Gradient Boosting.
     - arima: auto arima (available only after 4.9 deployed with Exodus, only available for regression).
-    - prophet: auto prophet (available only after 4.9 deployed with Exodus, only available for regression).
+    - prophet
+    (=metaprophet): auto prophet (available only after 4.9 deployed with Exodus, only available for regression).
     - theta: auto theta (available only after 4.9 deployed with Exodus, only available for regression).
     - ets: auto ets (available only after 4.10 deployed with Exodus, only available for regression).
+    - lgbm_accuracy(=lgbmaccuracy)
+    - lgbm_speed(=lgbmspeed)
+    - few_shot_learning(=fewshotlearning)
+    - lstm
     """
-    DRF = 'DRF'
-    GLM = 'GLM'
-    GBM = 'GBM'
-    DeepLearning = 'DeepLearning'
-    StackedEnsemble = 'StackedEnsemble'
-    XGBoost = 'XGBoost'
-    arima = 'arima'
-    prophet = 'prophet'
-    ets = 'ets'
-    theta = 'theta'
-    tpot = 'tpot'
-    lgbm_gpu = 'lgbm_gpu'
+
+    """
+    These are the algorithm names for swarm mode decanter
+    """
+    DRF = "DRF"
+    GLM = "GLM"
+    GBM = "GBM"
+    DeepLearning = "DeepLearning"
+    StackedEnsemble = "StackedEnsemble"
+    XGBoost = "XGBoost"
+    arima = "arima"
+    prophet = "prophet"
+    ets = "ets"
+    theta = "theta"
+    tpot = "tpot"
+    lgbm_gpu = "lgbm_gpu"
+    lgbm_speed = "lgbm_speed"
+    lgbm_accuracy = "lgbm_accuracy"
+    lstm = "lstm"
+    few_shot_learning = "few_shot_learning"
+
+    """
+    These are the algorithm names which is changed for K8S mode decanter
+    """
+    autotpot = "autotpot"
+    rfmulticlassifier = "rfmulticlassifier"
+    fewshotlearning = "fewshotlearning"
+    lgbmaccuracy = "lgbmaccuracy"
+    lgbmspeed = "lgbmspeed"
+    metaprophet = "metaprophet"
